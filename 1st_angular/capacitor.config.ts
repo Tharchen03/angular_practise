4e5b879a307fc8612cf48da3345f1ed265ac73ea
Tharchen03 <sherabten098@gmail.com>
2023-05-09 14:58:15 +0600

    angular lesson

---
+++ 1st_angular/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1st_angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
